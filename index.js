d2a0562fbe607bc19520942d7c5130fd1936ce55
yanakin394 <119351474+yanakin394@users.noreply.github.com>
2023-03-14 10:22:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+//Практическое задание
+// 1. Зарплата вечернего уборщика в городской поликлинике 40000 рублей, а педагога по вокалу 22000 рублей. 
+let cleaner = 40000;
+let teacher = 22000;
+console.log(cleaner + teacher);
+
+//2. Сложите слово 'Владивосток' и число 2000...
+let word = 'Владивосток';
+let year = 2000;
+console.log(word + " " + year);
+
+//3. Прокрастинируя дома, Русалка 128 минут....
+let messngr = 128;
+let likes = 254;
+let read = 137;
+let watch = 201;
+console.log((messngr + likes + read + watch) / 60 + 'hrs');
+
+//4. Возведите число 2 в 5 степень.
+console.log(2 ** 5);
+
+//5. Выведите в консоль строку...
+let alice = ('Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!');
+console.log(alice);
+
+//6. 
+const greetings = "Привет, ";
+const username = "Кот";
+alert(greetings + username);
+
+//7.
+let time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time = 19;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+time = 9;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву.')
+
+//8.
+let fahrenheit = 451;
+let celcius = (fahrenheit - 32) / 1.8;
+console.log(fahrenheit + ' градуса по Фаренгейту - это ' + celcius + ' градуса по Цельсию.');
+
+
+
